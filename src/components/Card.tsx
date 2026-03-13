0ab599ea0b4e055094bb53f5528edb6eeca610b3
Jeravima <jeravi61@gmail.com>
2026-03-13 10:45:05 -0500

    primero

---
+++ src/components/Card.tsx
@@ -0,0 +1,17 @@
+
+interface CardProps{
+    id: number 
+    name: string
+    gender: string
+    image: string
+}
+
+export const Card = ({id, name,gender,image}:CardProps) => {
+  return (
+    <div key={id} className="bg-white shadow-2xl p-3">
+        <img src={image} alt="" />
+        <h2 className="font-bold text-xl">{name}</h2>
+        <p>Gender: {gender}</p>
+    </div>
+  )
+}
